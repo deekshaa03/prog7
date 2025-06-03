8fe214496447e4bb5692b3ceef83b4c7203f6cdc
deekshaa03 <deekshakotian928@gmail.com>
2025-06-03 19:26:16 +0530

    first commit

---
+++ src/main/java/program2/program2/example/App.java
@@ -0,0 +1,13 @@
+package program2.program2.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
